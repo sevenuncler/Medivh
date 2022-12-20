ea044ac2e445dcf62f8a29722f3af9af01cb3c8f
fanghe <fanghe@bytedance.com>
2022-12-20 11:02:23 +0800

    init

---
+++ src/main/java/com/carry/carryouhome/CarryouhomeApplication.java
@@ -0,0 +1,13 @@
+package com.carry.carryouhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryouhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryouhomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carry/carryouhome/CarryouhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carry.carryouhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarryouhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
